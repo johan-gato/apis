fc968005d3615bd44fa72beb89fd85790250e886
Johan Espinoza <johan2003.espinozamail.com>
2025-05-30 02:04:19 -0400

    seguimospasoapaso

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENWEATHERMAP_API_KEY = os.getenv('OPENWEATHERMAP_API_KEY')
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")  
+
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
